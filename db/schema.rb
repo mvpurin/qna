86fd27d6ebf8d9e9d6edf9a9c016c54b15d36722
mvpurin <mvpurin@gmail.com>
2023-03-28 22:18:05 +0700

    add specs for Question and Answer models, add validations and associations

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_150442) do
   create_table "answers", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_150442) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "questions"
 end

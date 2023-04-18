87984000e39a59fe8b6b707628f7260b7ec7cf61
mvpurin <mvpurin@gmail.com>
2023-04-18 21:20:40 +0700

    add unit-test for new_method in answers_controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,11 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   before_action :find_question, only: %i[index new create]
 
+  def new
+    @answer = @question.answers.new
+  end
+  
   def create
     @answer = @question.answers.new(answer_params)
 

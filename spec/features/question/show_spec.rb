c6f05049fd337da892ae8e2ddf7ed9f17f45da33
mvpurin <mvpurin@gmail.com>
2023-04-22 17:17:35 +0700

    Acceptance test for 'User see question and answer'

---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User can see a question and its answers', %q{
+  As an authenticated user or not
+  I would like to see a question and its answers
+} do
+  given!(:question) { create(:question) }
+  given!(:answer) { create(:answer, question: question) }
+
+  scenario 'User see question and its answers' do
+    visit question_path(question)
+
+    expect(page).to have_content "#{question.title}"
+    expect(page).to have_content "#{question.body}"
+
+    expect(page).to have_content "#{answer.title}"
+    expect(page).to have_content "#{answer.body}"
+  end
+end

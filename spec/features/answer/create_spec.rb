fb9c2ac77d23955ee8880330d662162920731986
mvpurin <mvpurin@gmail.com>
2023-04-28 22:49:24 +0700

    problems with js

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -7,7 +7,7 @@ feature 'User can create answer', '
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
 
-  describe 'Authenticated user' do
+  describe 'Authenticated user', js: true do
     background do
       sign_in(user)
       visit question_path(question)
@@ -18,6 +18,7 @@ feature 'User can create answer', '
       fill_in 'Body', with: 'Answer body'
       click_on 'Give answer'
 
+      expect(current_path).to eq question_path(question)
       expect(page).to have_content 'Your answer was successfully created.'
       expect(page).to have_content 'Answer title'
       expect(page).to have_content 'Answer body'
@@ -25,12 +26,12 @@ feature 'User can create answer', '
 
     scenario 'tries to give an answer with errors' do
       click_on 'Give answer'
-save_and_open_page
+
       expect(page).to have_content "Title can't be blank"
     end
   end
 
-  scenario 'Unauthenticated user tries to give an answer' do
+  scenario 'Unauthenticated user tries to give an answer', js: true do
     visit question_path(question)
 
     click_on 'Give answer'

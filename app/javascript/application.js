a1b6105f44b5c4733bdae57f2b3b2d1d575b119f
mvpurin <mvpurin@gmail.com>
2023-05-01 23:51:03 +0700

    Add jQuery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-require("turbolinks")
-require("@rails/activestorage")
-require("channels")
-require("@rails/ujs")
-require("jquery")
+// https://stackoverflow.com/questions/70921378/how-to-install-jquery-and-bootstrap-in-rails-7-app-using-esbuild-without-webpac
+import "jquery"
+import "jquery_ujs"

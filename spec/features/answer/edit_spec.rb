4c0dbb25ed538893f7ee492c88fc93c6d866a1a3
mvpurin <mvpurin@gmail.com>
2023-05-15 22:40:35 +0700

    Done edit_spec.rb for answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -6,8 +6,10 @@ feature 'User can edit his answer', '
   I would like to be able to edit my answer
 ' do
   given!(:user) { create(:user) }
+  given!(:user2) { create(:user) } 
   given!(:question) { create(:question, user: user) }
   given!(:answer) { create(:answer, question: question, user: user) }
+  given!(:answer2) { create(:answer, question: question, user: user2) }
 
   scenario 'Unauthenticated user can not edit answer' do
     visit question_path(question)
@@ -15,14 +17,15 @@ feature 'User can edit his answer', '
     expect(page).to_not have_link 'Edit'
   end
 
-  describe 'Authenticated user' do
-    scenario 'edits his answer', js: true do
+  describe 'Authenticated user', js: true do
+    background do
       sign_in(user)
-      visit question_path(question)
+      visit question_path(question)   
+    end
 
-      click_on 'Edit'
-
-      within '.answers' do
+    scenario 'edits his answer' do
+      within "div#answer-#{answer.id}" do
+        click_on 'Edit'
         fill_in 'Title', with: 'edited answer title'
         fill_in 'Body', with: 'edited answer body'
         click_on 'Save'
@@ -34,7 +37,20 @@ feature 'User can edit his answer', '
       end
     end
 
-    scenario 'edits his answer with errors'
-    scenario "tries to edit other user's answer"
+    scenario 'edits his answer with errors' do
+      within "div#answer-#{answer.id}" do
+        click_on 'Edit'
+        fill_in 'Title', with: ''
+        click_on 'Save' 
+      end
+
+      expect(page).to have_content "Title can't be blank"
+    end
+
+    scenario "tries to edit other user's answer" do
+      within "div#answer-#{answer2.id}" do
+        expect(page).to_not have_link 'Edit'
+      end
+    end
   end
 end

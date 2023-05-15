0a47d6c6a3177973ab60fe5a79385c2a8489704c
mvpurin <mvpurin@gmail.com>
2023-05-15 22:13:12 +0700

    Done edit_spec.rb

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -5,7 +5,9 @@ feature 'User can edit his question', %q{
   I wuold like to edit my question
 } do
   given!(:user) { create(:user) }
+  given!(:user2) { create(:user) }
   given!(:question) { create(:question, user: user) }
+  given!(:question2) { create(:question, user: user2) }
 
   scenario 'Unauthenticated user tries to edit answer' do
     visit questions_path
@@ -16,13 +18,13 @@ feature 'User can edit his question', %q{
   describe 'Authenticated user', js: true do
     background do
       sign_in(user)
-      visit questions_path
+      visit questions_path 
     end
   
     scenario 'tries to edit his question' do
       click_on 'Edit question'
 
-      within '.questions' do
+      within "div#question-#{question.id}" do
         fill_in 'Title', with: 'edited question title'
         fill_in 'Body', with: 'edited question body'
         click_on 'Save'
@@ -33,12 +35,24 @@ feature 'User can edit his question', %q{
         expect(page).to_not have_selector 'textarea'
       end
     end
+  
+    scenario 'tries to edit his question with errors' do
+      click_on 'Edit question'
 
+      within "div#question-#{question.id}" do
+        fill_in 'Title', with: ''
+        click_on 'Save'
+      end
 
+      expect(page).to have_content "Title can't be blank"
+    end
 
+    scenario "tries to edit other user's question" do
+      visit questions_path
 
-
-    scenario 'tries to edit his question with answers'
-    scenario "tries to edit other user's question"
+      within "div#question-#{question2.id}" do  
+        expect(page).to_not have_content 'Edit question'
+      end    
+    end
   end
 end

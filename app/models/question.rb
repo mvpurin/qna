8cd0855a3b09247e328595c2fc9c4510d4635c28
mvpurin <mvpurin@gmail.com>
2023-05-20 22:36:37 +0700

    test for question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  belongs_to :best_answer, class_name: 'Answer', foreign_key: 'best_answer_id'
   belongs_to :user
 
   validates :title, :body, presence: true

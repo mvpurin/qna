da61ac1ce493733abbe0753068babf53abfec61a
mvpurin <mvpurin@gmail.com>
2023-05-28 18:02:07 +0700

    add multiple files

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -35,11 +35,12 @@ feature 'User can create question', '
       fill_in 'Title', with: 'Text question'
       fill_in 'Body', with: 'text text text'
 
-      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
       click_on 'Ask'
 
       expect(page).to have_link 'rails_helper.rb'
-     end
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'Unauthenticated user tries to ask a question' do
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Question, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:best_answer).optional }
 
-  it 'have one attached file' do
-    expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One)
+  it 'have many attached files' do
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
 end

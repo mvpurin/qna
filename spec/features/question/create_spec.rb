0360202bcce3846c893c205c068ec7536460479f
mvpurin <mvpurin@gmail.com>
2023-05-29 00:24:49 +0700

    Add files to question while editing

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -10,7 +10,6 @@ feature 'User can create question', '
   describe 'Authenticated user' do
     background do
       sign_in(user)
-
       visit questions_path
       click_on 'Ask question'
     end

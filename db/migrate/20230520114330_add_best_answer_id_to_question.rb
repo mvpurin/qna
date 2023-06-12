2ecbd35fb37b8e9f6f3c16d797756ea9cb5dae88
mvpurin <mvpurin@gmail.com>
2023-06-12 15:02:32 +0700

    User can delete best answer error resolved

--- db/migrate/20230520114330_add_best_answer_id_to_question.rb
+++ db/migrate/20230520114330_add_best_answer_id_to_question.rb
@@ -1,5 +1,5 @@
 class AddBestAnswerIdToQuestion < ActiveRecord::Migration[7.0]
   def change
-    add_reference :questions, :best_answer, foreign_key: { to_table: :answers }, index: true
+    add_reference :questions, :best_answer, foreign_key: { to_table: :answers, on_delete: :nullify }, index: true
   end
 end

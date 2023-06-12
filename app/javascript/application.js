46dd4ec22045d0bdec0ca037721a080be55ea6c6
mvpurin <mvpurin@gmail.com>
2023-06-12 11:17:46 +0700

    Bootstrap installed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,8 +5,12 @@ import "controllers"
 // https://stackoverflow.com/questions/70921378/how-to-install-jquery-and-bootstrap-in-rails-7-app-using-esbuild-without-webpac
 import "jquery"
 import "jquery_ujs"
+import "popper"
+import "bootstrap"
+
 import "@rails/activestorage"
 
+// Custom JS files
 import "utilities/edit_answer"
 import "utilities/edit_question"
 import "utilities/delete_file"

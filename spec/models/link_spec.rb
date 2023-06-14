0061b8a54dd7a769b9f431711db2336993b95801
mvpurin <mvpurin@gmail.com>
2023-06-14 14:40:30 +0700

    Add validation URL for links

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  it { should allow_value('http://example.com').for(:url) }
+  it { should allow_value('https://example.com').for(:url) }
 end

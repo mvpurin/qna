c489dd4abbc97795d152626e14cbbae24310d49f
mvpurin <mvpurin@gmail.com>
2023-07-12 17:13:34 +0700

    All tests except shared examples

--- spec/features/answer/vote_spec.rb
+++ spec/features/answer/vote_spec.rb
@@ -1,44 +1,45 @@
 require 'rails_helper'
 
-feature 'User can vote for question', '
+feature 'User can vote for an answer', '
 As an authenticated user I would like
-to be able to vote for a question I like
+to be able to vote for an answer I like
 ' do
   given(:user) { create(:user) }
   given(:user_2) { create(:user) }
-  given!(:question) { create(:question, user: user) }
-  given!(:question_2) { create(:question, user: user_2) }
+  given(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, user: user, question: question) }
+  given!(:answer_2) { create(:answer, user: user_2, question: question) }
 
   describe 'Authenticated user', js: true do
     background do
-      sign_in(user_2)
-      visit questions_path
+      sign_in(user)
+      visit question_path(question)
     end
 
-    scenario 'votes for a question' do
-      within "div#question-#{question.id}" do
+    scenario 'votes for an answer' do
+      within "div#answer-#{answer_2.id}" do
         click_on 'Like'
         expect(page).to have_content 'Rating: 1'
       end
     end
 
-    scenario 'votes against a question' do
-      within "div#question-#{question.id}" do
+    scenario 'votes against an answer' do
+      within "div#answer-#{answer_2.id}" do
         click_on 'Dislike'
         expect(page).to have_content 'Rating: -1'
       end
     end
 
-    scenario 'votes for a question twice' do
-      within "div#question-#{question.id}" do
+    scenario 'votes for an answer twice' do
+      within "div#answer-#{answer_2.id}" do
         click_on 'Like'
         click_on 'Like'
         expect(page).to have_content 'Rating: 1'
       end
     end
 
-    scenario 'votes for his question' do
-      within "div#question-#{question_2.id}" do
+    scenario 'votes for his answer' do
+      within "div#answer-#{answer.id}" do
         click_on 'Like'
         expect(page).to have_content 'Rating: 0'
       end

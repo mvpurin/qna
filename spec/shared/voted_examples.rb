e2890897c492ffdf2d53ff99860002d5c6b59103
mvpurin <mvpurin@gmail.com>
2023-07-16 22:46:43 +0700

    Changes in voted_examples

--- spec/shared/voted_examples.rb
+++ spec/shared/voted_examples.rb
@@ -1,18 +1,38 @@
 require 'rails_helper'
 
 RSpec.shared_examples "voted" do
-  before { login(user) }
+  let(:user_2) { create(:user) }
+  before { login(user_2) }
 
   describe "GET #vote_for" do
-    before { get :vote_for, params: { id: votable, format: :json } }
+    before { get :vote_for, params: { vote: attributes_for(:vote), id: votable, user: user_2, format: :json } }
 
     it 'gets json response' do
       expect(response.header['Content-Type']).to include 'application/json'
       expect(response.body).to eq (assigns(:votable)).to_json
+      expect(response.status).to eq 200
     end
 
     it 'gets forbidden status if user has already voted' do
-      get :vote_for, params: { id: votable.id, format: :json }
+      get :vote_for, params: { vote: attributes_for(:vote), id: votable, user: user_2, format: :json }
+
+      expect(response.header['Content-Type']).to include 'application/json'
+      expect(response.body).to eq (assigns(:votable)).to_json
+      expect(response.status).to eq 403
+    end
+  end
+
+  describe "GET #vote_against" do
+    before { get :vote_against, params: { vote: attributes_for(:vote), id: votable, user: user_2, format: :json } }
+
+    it 'gets json response' do
+      expect(response.header['Content-Type']).to include 'application/json'
+      expect(response.body).to eq (assigns(:votable)).to_json
+      expect(response.status).to eq 200
+    end
+
+    it 'gets forbidden status if user has already voted' do
+      get :vote_against, params: { vote: attributes_for(:vote), id: votable, user: user_2, format: :json }
 
       expect(response.header['Content-Type']).to include 'application/json'
       expect(response.body).to eq (assigns(:votable)).to_json

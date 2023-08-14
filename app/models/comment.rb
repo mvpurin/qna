fc3341e9e7ea3993b8fce8475ea86f43bbc81c99
mvpurin <mvpurin@gmail.com>
2023-08-14 12:47:01 +0700

    Streaming comments for questions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,21 @@
 class Comment < ApplicationRecord
+  after_create :publish_comment
+
   belongs_to :user, optional: true
   belongs_to :commentable, polymorphic: true, optional: true
 
   validates :body, presence: true
+
+  def publish_comment
+    return if self.errors.any?
+
+    question_id = self.commentable_type == "Question" ? self.commentable.id : self.commentable.question.id
+
+    ActionCable.server.broadcast(
+      "comments-question-#{question_id}",
+      {
+        comment: self
+      }
+    )
+  end
 end

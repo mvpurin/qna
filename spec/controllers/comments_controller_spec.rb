35f02081832b4077ea0ada08a09e50bf8ef0117d
mvpurin <mvpurin@gmail.com>
2023-08-16 16:51:28 +0700

    Tests for comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,10 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  describe 'GET #create' do
-    it 'returns http success' do
-      get :create
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
+  let(:answer) { create(:answer, question: question, user: user) }
+
+  describe 'POST #create' do
+    before { login(user) }
+
+    context 'with valid attributes' do
+      it 'saves a new question comment to database' do
+        expect do
+          post :create, params: { comment: attributes_for(:comment), question_id: question, user_id: user }
+        end.to change(Comment, :count).by(1)
+      end
+
+      it 'saves a new answer comment to database' do
+        expect do
+          post :create, params: { comment: attributes_for(:comment), answer_id: answer, user_id: user }
+        end.to change(Comment, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the comment' do
+        expect do
+          post :create,
+              params: { comment: attributes_for(:comment, :invalid), question_id: question, user_id: user }
+        end.not_to change(Comment, :count)
+      end
+
+      it 'does not save the comment' do
+        expect do
+          post :create,
+              params: { comment: attributes_for(:comment, :invalid), answer_id: answer, user_id: user }
+        end.not_to change(Comment, :count)
+      end
     end
   end
 end

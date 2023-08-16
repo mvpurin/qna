f4a9f3400032cad8a37c7da61691ec2ee97f2b36
mvpurin <mvpurin@gmail.com>
2023-08-16 15:33:47 +0700

    Remove nested attributes from commentable

--- spec/shared/commentable_examples.rb
+++ spec/shared/commentable_examples.rb
@@ -3,6 +3,5 @@ require 'rails_helper'
 RSpec.shared_examples 'commentable' do
   describe described_class, type: :model do
     it { should have_many(:comments).dependent(:destroy) }
-    it { should accept_nested_attributes_for :comments }
   end
 end

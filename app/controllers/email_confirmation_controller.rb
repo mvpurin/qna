d749e43551e63ea9a7681626bf0c7a2cd8a418dc
mvpurin <mvpurin@gmail.com>
2023-08-24 11:58:06 +0700

    Add controller for email confirmation

---
+++ app/controllers/email_confirmation_controller.rb
@@ -0,0 +1,19 @@
+class EmailConfirmationController < ApplicationController
+  before_action :require_no_authentication
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+end

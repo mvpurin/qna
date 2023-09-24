cb95a2584dee5d7cb32a7b102684fa96adf07c31
mvpurin <mvpurin@gmail.com>
2023-09-24 11:51:14 +0700

    delete useless migrations

--- db/migrate/20230824081432_add_email_confirmation_token_and_email_confirmation_token_sent_at_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddEmailConfirmationTokenAndEmailConfirmationTokenSentAtToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :email_confirmation_token, :string
-    add_index :users, :email_confirmation_token
-    add_column :users, :email_confirmation_token_sent_at, :datetime
-  end
-end
--- db/migrate/20230825165531_remove_column_confirmed_email_value_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnConfirmedEmailValueFromUsers < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :confirmed_email_value
-  end
-end

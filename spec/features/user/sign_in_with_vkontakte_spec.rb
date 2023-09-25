4d3c6aafaad4ac9ce4fde1d515405e92a82481a3
mvpurin <mvpurin@gmail.com>
2023-09-25 23:39:39 +0700

    Acceptance tests for Vk and Github authorization

--- spec/features/user/sign_in_with_vkontakte_spec.rb
+++ spec/features/user/sign_in_with_vkontakte_spec.rb
@@ -2,30 +2,47 @@ require 'rails_helper'
 
 feature 'Sign in with Vkontakte', '
 User is able to sign in or sign up with his Vkontakte account ' do
-  background do
-    OmniAuth.config.test_mode = true
-  end
+  given(:user) { create(:user) }
 
-  given!(:user) { create(:user) }
-
-  describe 'request has email' do
-    scenario 'existed user tries to sign in' do
-      OmniAuth.config.add_mock(:vkontakte, uid: '12345', info: { email: user.email })
-      visit new_user_session_path
-      click_on 'Sign in with Vkontakte'
-      expect(page).to have_content 'Successfully authenticated from Vkontakte account.'
-    end
-
-    scenario 'new user tries to sign up' do
+  context 'new user' do
+    scenario 'provider provides email' do
       OmniAuth.config.add_mock(:vkontakte, uid: '12345', info: { email: 'new_user@email.com' })
       visit new_user_session_path
       click_on 'Sign in with Vkontakte'
-      expect(page).to have_content 'Successfully authenticated from Vkontakte account.'
+      open_email('new_user@email.com')
+      current_email.click_link 'Confirm my account'
+      expect(page).to have_content 'Your email address has been successfully confirmed.'
+    end
+
+    scenario 'provider does not provide email' do
+      OmniAuth.config.add_mock(:vkontakte, uid: '12345', info: { email: nil })
+      visit new_user_session_path
+      click_on 'Sign in with Vkontakte'
+      fill_in 'Email', with: 'new_user@email.com'
+      click_on 'Send email'
+      open_email('new_user@email.com')
+      current_email.click_link 'Confirm my account'
+      expect(page).to have_content 'Your email address has been successfully confirmed.'
     end
   end
 
-  describe 'request does not have email' do
-    scenario 'existed user tries to sign in'
-    scenario 'new user tries to sign up'
+  context 'existing user' do
+    scenario 'provider provides email' do
+      OmniAuth.config.add_mock(:vkontakte, uid: '12345', info: { email: user.email })
+      user.update(confirmed_at: DateTime.now)
+      visit new_user_session_path
+      click_on 'Sign in with Vkontakte'
+      expect(page).to have_content 'Successfully authenticated from Vkontakte account.'
+    end
+
+    scenario 'provider does not provide email' do
+      user.update(confirmed_at: DateTime.now)
+      OmniAuth.config.add_mock(:vkontakte, uid: '12345', info: { email: nil })
+      visit new_user_session_path
+      click_on 'Sign in with Vkontakte'
+      fill_in 'Email', with: user.email
+      click_on 'Send email'
+      expect(page).to have_content 'Please try to sign in with your email and password'
+    end
   end
 end

b2d81411ff2e9317412473f59604e98110eff340
mvpurin <mvpurin@gmail.com>
2023-09-29 18:04:31 +0700

    improve all tests

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -116,9 +116,8 @@ feature 'User can create question', '
   describe 'Unauthenticated user' do
     scenario 'tries to ask a question' do
       visit questions_path
-      click_on 'Ask question'
 
-      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      expect(page).to_not have_content 'Ask question'
     end
 
     scenario 'tries to add comment' do

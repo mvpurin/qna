4b4c2e22e4dd20c9ff2a35bdf71c10f511380988
mvpurin <mvpurin@gmail.com>
2023-10-02 22:53:00 +0700

    API for list of users except authenticated one and specs

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::ProfilesController < Api::V1::BaseController
-  skip_authorization_check only: [:me]
+  skip_authorization_check only: [:me, :all]
   
   def me
     render json: current_resource_owner
+  end
+
+  def all
+    @users = User.where.not(id: current_resource_owner)
+    render json: @users
   end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -25,8 +25,49 @@ describe 'Profiles API', type: :request do
 
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json['user'][attr]).to eq me.send(attr).as_json
         end
+      end
+
+      it 'does not return private fields' do
+        %w[password encrypted_password].each do |attr|
+          expect(json).to_not have_key(attr)
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles/all' do
+    let(:api_path) { '/api/v1/profiles/all' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 3) }
+      let(:user) { users.first }
+      let(:other_user) { users.last }
+      let(:user_response) { json['users'].last }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id) }
+
+      before do
+        get api_path, params: { access_token: access_token.token }, headers: headers
+      end
+
+      it 'returns 200 status' do 
+        expect(response).to be_successful
+      end
+
+      it 'returns all public fields' do
+        %w[id email admin created_at updated_at].each do |attr|
+          expect(user_response[attr]).to eq other_user.send(attr).as_json
+        end
+      end
+
+      it 'returns list of all users except authenticated one' do
+        expect(json['users'].size).to eq 2
+        expect(json['users']).to_not include(user)
       end
 
       it 'does not return private fields' do

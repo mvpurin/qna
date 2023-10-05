4a302dd19f70b8c05341aecb968c3dd5ad2e17da
mvpurin <mvpurin@gmail.com>
2023-10-05 10:07:13 +0700

    API for answer with comments, files and links; and specs

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  skip_authorization_check only: [:index]
+  skip_authorization_check only: [:index, :show]
   
   def index
     @answers = Answer.where(question_id: params[:question_id])
     render json: @answers, each_serializer: AnswersSerializer
+  end
+
+  def show
+    @answer = Answer.find(params[:id])
+    render json: @answer, serializer: AnswerSerializer
   end
 end

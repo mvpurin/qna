eec8bf94d019f8016fb879d672301e4a33f5ea97
mvpurin <mvpurin@gmail.com>
2023-10-06 16:08:16 +0700

    API for delete question; and specs

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  skip_authorization_check only: [:index, :show]
+  skip_authorization_check only: [:index, :show, :destroy]
   
   def index
     @answers = Answer.where(question_id: params[:question_id])
@@ -9,5 +9,11 @@ class Api::V1::AnswersController < Api::V1::BaseController
   def show
     @answer = Answer.find(params[:id])
     render json: @answer, serializer: AnswerSerializer
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+    head :no_content
   end
 end

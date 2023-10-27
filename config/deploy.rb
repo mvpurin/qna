cf729b6d08860128c8ab058f48ca870300b35869
mvpurin <mvpurin@gmail.com>
2023-10-27 22:17:36 +0700

    more..

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_user, 'deployer'
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/master.key"
-append :linked_files, %w{config/database.yml config/master.key config/credentials/production.key}
+append :linked_files, "config/database.yml", "config/master.key", "config/credentials/production.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
